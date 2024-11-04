9b56d937eafeddc73d94f1e812eef5a7a2d36907
Kuzuri247 <mindsetmatters47@gmail.com>
2024-11-04 12:44:43 +0530

    added links

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
-
 import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import { Router } from 'lucide-react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Liquidity from './components/Liquidity'
+import Swap from './components/Swap'
+import Portfolio from './components/Portfolio'
+import More from './components/More'
+
+
+
 function App() {
   
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <App />,
+    },
+    {
+      path: '/swap',
+      element: < Swap/>,
+      errorElement: <App />,
+    },
+    {
+      path: '/liquidity',
+      element: <Liquidity />,
+      errorElement: <App />,
+    },
+    {
+      path: '/more ',
+      element: <More/>,
+      errorElement: <App />,
+    }
+  ])
 
   return (
-    <div className=" text-3xl">
+    <div className=" text-3xl font-bold text-center">
       <Navbar/>
-      Hello World
+      Billa 69
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,19 @@
+import { create } from 'domain'
 import React from 'react'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 const Navbar = () => {
+  
+  
+
   return (
     <div>
-        <h1>Navbar</h1>
-        <p>Navbar</p>
-        
+        <nav  className="flex justify-center space-x-4 py-7">
+          <a href="src/components/Swap.tsx">Swap</a>
+          <a href="src/components/Liquidity.tsx">Liquidity</a>
+          <a href="src/components/Portfolio.tsx">Portfolio</a> 
+          <a href="src/components/More.tsx">More</a> 
+        </nav>
     </div>
   )
 }
