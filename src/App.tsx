7f45b1c085739204be964aef36864b19538f318e
Kuzuri247 <mindsetmatters47@gmail.com>
2024-11-04 11:39:19 +0530

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 
+import React = require('react')
 import './App.css'
-
+import Navbar from './components/Navbar'
 function App() {
   
 
   return (
     <div className=" text-3xl">
+      <Navbar/>
       Hello World
     </div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.js'
+import App from './App.tsx'
+import React = require('react')
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  
     <App />
-  </StrictMode>,
+  
 )
