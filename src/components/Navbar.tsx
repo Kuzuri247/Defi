a0a35394ff4688c575923b1b0bc14b6b9291737e
0x00zer0day <0x00zer0day@gmail.com>
2024-11-04 17:15:36 +0530

    Fixed routes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,17 +3,15 @@ import React from 'react'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 const Navbar = () => {
-  
-  
 
   return (
     <div>
         <nav  className="flex justify-center space-x-4 py-7 text-3xl font-bold">
 
-          <Link to="src/components/Swap.tsx">Swap</Link>
-          <Link to="src/components/Liquidity.tsx">Liquidity</Link>
-          <Link to="src/components/Portfolio.tsx">Portfolio</Link> 
-          <Link to="src/components/More.tsx">More</Link> 
+          <Link to="/swap">Swap</Link>
+          <Link to="/liquidity">Liquidity</Link>
+          <Link to="/portfolio">Portfolio</Link> 
+          <Link to="/more">More</Link> 
         </nav>
     </div>
   )
