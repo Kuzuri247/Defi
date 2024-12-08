eb0b33a5efd1882c5a4912861f71f7648a35f6b1
Kuzuri247 <mindsetmatters47@gmail.com>
2024-12-08 21:47:39 +0530

    provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./index.css";
-import { Provider } from "@/components/ui/provider"
+import { Provider } from "./components/ui/provider"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
